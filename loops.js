fcc6d4d1787fa6210cf5590adb46e40bf62ab1c6
annaezer <annaezer@gmail.com>
2022-10-12 13:26:54 +0200

    loops and arrays af

--- loops.js
+++ loops.js
@@ -66,7 +66,7 @@ for (let i = 0; i < 16; i++) {
 for (let i = 0; i < 10; i++) {
     if (i < 3) {
         console.log(i);
-    } else if ((i > 2) && (i < 6)) {
+    } else if (i < 6) {
         console.log('>' + i);
     } else {
         console.log('>>' + i);
